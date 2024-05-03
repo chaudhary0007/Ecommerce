c7e929a26c4cdda9f7c88815a2ce28ed7b8085b0
shobhit <shobhitmishra2002@gmail.com>
2024-05-03 21:10:38 +0530

    feat: added support for morgan middleware

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,27 @@
-// const express = require("express");
-// const bodyParser = require("body-parser")
-import express from 'express';
+import express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/app.route.js";
-import loggerMiddleware from './middleware/app.log.js';
+// import loggerMiddleware from "./middleware/app.log.js";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 3000;
-app.use(loggerMiddleware);
+
+// you can place your middleware here..
+app.use(morgan("dev"));
+// app.use(loggerMiddleware);
 app.use(bodyParser.json());
 
+// then place your routes
+app.use("/", router);
 
+// then, finally start listening of server
 app.listen(PORT, (error) => {
   if (!error)
-  console.log(
+    console.log(
       "Server is Successfully Running, and App is listening on port " + PORT
     );
   else console.log("Error occurred, server can't start", error);
-})
+});
 
-app.use('/', router)
 export default app;
-// const router = express.Router();
-// router.get("/", (req, res) => {
-  //   res.send("Hello")
-  //   console.log('Method :',req.method,'Path:',req.path)
-  // });
-  // export default router;

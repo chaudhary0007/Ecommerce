6136ae6408c9b4ee15a67ea243b0e61c369c9959
Atul Chaudhary <ofcodegame11@gmail.com>
2024-05-09 17:44:37 +0530

    add user #8

--- backend/schema/User.js
+++ backend/schema/User.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  name: String,
+  FirstName:String,
+  LastName: String,
+  email: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true }
 });
 
 const User = mongoose.model("User", userSchema);
